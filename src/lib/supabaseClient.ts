3afbc5c86cc436dddfef0ba240598eaf6e29cb19
MortalFlame21 <markgio2100@gmail.com>
2025-10-25 15:47:53 +1100

    fix anon

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const anon_key = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const anon_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 const supabase = createClient(supabase_url, anon_key)
 
